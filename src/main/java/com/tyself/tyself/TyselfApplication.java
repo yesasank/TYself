c43adeb6811288c5b836eabf10748dcb437a1f44
Sasank Mahadev <yesasank@gmail.com>
2026-06-18 19:42:17 +0530

    Initial Spring Boot setup with PostgreSQL

---
+++ src/main/java/com/tyself/tyself/TyselfApplication.java
@@ -0,0 +1,13 @@
+package com.tyself.tyself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyselfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyselfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tyself/tyself/TyselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyself.tyself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyselfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
