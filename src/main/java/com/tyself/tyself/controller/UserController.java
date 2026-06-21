eeb0d73ea045dcc8e53f0a7462eb796d36d8a666
Sasank Mahadev <yesasank@gmail.com>
2026-06-21 18:12:03 +0530

    Completed User CRUD module

--- src/main/java/com/tyself/tyself/controller/UserController.java
+++ src/main/java/com/tyself/tyself/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.tyself.tyself.controller;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +31,17 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
     }
 }
--- src/main/java/com/tyself/tyself/service/UserService.java
+++ src/main/java/com/tyself/tyself/service/UserService.java
@@ -21,4 +21,22 @@ public class UserService {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+    public User updateUser(Long id, User updatedUser) {
+        User existingUser = userRepository.findById(id).orElse(null);
+
+        if (existingUser != null) {
+            existingUser.setName(updatedUser.getName());
+            existingUser.setEmail(updatedUser.getEmail());
+
+            return userRepository.save(existingUser);
+        }
+
+        return null;
+    }
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
